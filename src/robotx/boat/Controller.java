22a83960a7724eb94c9853913a2219b6736ef4d3
Dhrubajit <dhrubajit1992@gmail.com>
2014-10-11 22:30:21 +0800

    getDistance updated

--- src/robotx/boat/Controller.java
+++ src/robotx/boat/Controller.java
@@ -38,14 +38,14 @@ public class Controller implements Runnable {
   public double getBearingError() {
     //reference https://gist.github.com/jeromer/200558
 
-    pointA = [current_latitude, current_longitude];
-    pointB = [destination_latitude, destination_longitude];
-		pointA[0] = Math.toRadians(pointA[0])
-		pointA[1] = Math.toRadians(pointA[1])
-		pointB[0] = Math.toRadians(pointB[0])
-		pointB[1] = Math.toRadians(pointB[1])
-		double lat1 = pointA[0]
-		double lat2 = pointB[0]
+    public double [] pointA = {0,0};
+    public double [] pointB = {0,0};
+		pointA[0] = Math.toRadians(current_latitude);
+		pointA[1] = Math.toRadians(current_longitude);
+		pointB[0] = Math.toRadians(destination_latitude);
+		pointB[1] = Math.toRadians(destination_longitude);
+		double lat1 = pointA[0];
+		double lat2 = pointB[0];
 		double diffLong = pointB[1]-pointA[1]
 	
 		double x = Math.sin(diffLong)*Math.cos(lat2)
@@ -57,6 +57,74 @@ public class Controller implements Runnable {
   }
 
   public double getDistanceError() {
+		double lat1 = current_latitude;
+		double lat2 = destination_latitude;
+		double lon1 = current_longitude;
+		double lon2 = current_longitude;
+		double a = 6378137, b = 6356752.314245, f = 1 / 298.257223563;
+		double L = Math.toRadians(lon2 - lon1);
+		double U1 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat1)));
+		double U2 = Math.atan((1 - f) * Math.tan(Math.toRadians(lat2)));
+		double sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
+		double sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
+		double cosSqAlpha;
+		double sinSigma;
+		double cos2SigmaM;
+		double cosSigma;
+		double sigma;
+
+		double lambda = L, lambdaP, iterLimit = 100;
+		do 
+		{
+			double sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
+			sinSigma = Math.sqrt(	(cosU2 * sinLambda)
+									* (cosU2 * sinLambda)
+									+ (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda)
+									* (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda)
+								);
+			if (sinSigma == 0) 
+			{
+				return 0;
+			}
+
+			cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
+			sigma = Math.atan2(sinSigma, cosSigma);
+			double sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+			cosSqAlpha = 1 - sinAlpha * sinAlpha;
+			cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
+
+			double C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
+			lambdaP = lambda;
+			lambda = 	L + (1 - C) * f * sinAlpha	
+						* 	(sigma + C * sinSigma	
+								* 	(cos2SigmaM + C * cosSigma
+										* 	(-1 + 2 * cos2SigmaM * cos2SigmaM)
+									)
+							);
+		
+		} while (Math.abs(lambda - lambdaP) > 1e-12 && --iterLimit > 0);
+
+		if (iterLimit == 0) 
+		{
+			return 0;
+		}
+
+		double uSq = cosSqAlpha * (a * a - b * b) / (b * b);
+		double A = 1 + uSq / 16384
+				* (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+		double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+		double deltaSigma = 
+					B * sinSigma
+						* (cos2SigmaM + B / 4
+							* (cosSigma 
+								* (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM
+									* (-3 + 4 * sinSigma * sinSigma)
+										* (-3 + 4 * cos2SigmaM * cos2SigmaM)));
+		
+		double s = b * A * (sigma - deltaSigma);
+		
+		return s;
+
   }
 
   public void control() {
